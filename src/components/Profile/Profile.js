b2fc5a11cad218b90eab1bd1e5e22f1847a23867
Diana Kondakova <greys.mouse@yandex.ru>
2021-08-10 18:05:47 +0300

    adaptive Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import './profile.css';
+import './profile__form-container.css';
+import './profile__title.css';
+import './profile__form.css';
+import './profile__logout-link.css';
 
 import ProfileForm from '../ProfileForm/ProfileForm';
 
 function Profile() {
   return (
     <section className="profile">
-      <ProfileForm />
+      <div className="profile__form-container">
+        <h2 className="profile__title">Привет, Виталий!</h2>
+        <ProfileForm addClasses="profile__form" />
+        <Link className="profile__logout-link">Выйти из аккаунта</Link>
+      </div>      
     </section>
   );
 }
--- src/components/ProfileForm/ProfileForm.js
+++ src/components/ProfileForm/ProfileForm.js
@@ -2,22 +2,19 @@ import './profile-form.css';
 import './profile-form__input.css';
 import './profile-form__input-label.css';
 import './profile-form__input-label_last.css';
-import './profile-form__input-span.css';
 import './profile-form__submit-button.css';
 
-function ProfileForm() {
+function ProfileForm(props) {
   return (
-    <form className="profile-form">
+    <form className={"profile-form " + (props.addClasses || "")}>
       <label className="profile-form__input-label">
         Имя
         <input className="profile-form__input" />
       </label>
-      <span className="profile-form__input-span">Error</span>
       <label className="profile-form__input-label profile-form__input-label_last">
         E-mail
         <input className="profile-form__input" />
       </label>
-      <span className="profile-form__input-span">Error</span>
       <button className="profile-form__submit-button">Редактировать</button>
     </form>
   );
